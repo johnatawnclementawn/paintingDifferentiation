83daa9910db0e324795ce7f57538da20ccb41970
johnatawnclementawn <johnathan.clementi1@gmail.com>
2021-10-02 17:26:43 -0400

    Finished tallHills lowValleys, working on roughness

--- paintingAnalysis.py
+++ paintingAnalysis.py
@@ -20,9 +20,15 @@ arcpy.CheckOutExtension("Spatial")
 os.chdir(r'D:\Users\Johnathan\Google Drive\Grad School\PennDesign_MUSA\RasterGIS_UrbanAndEnvironmentalModeling\Week5\wk5Homework')
 path = os.getcwd()
 
-# Read file:
-pic1 = path + "\\Week05DataOnPaintings.gdb\\a"
+# Read files:
+# Data location:
+dataGDB = "\\Week05DataOnPaintings.gdb"
+# Array of pictures to process:
+pics = [path+dataGDB+"\\a",path+dataGDB+"\\b",path+dataGDB+"\\c",path+dataGDB+"\\d",path+dataGDB+"\\e"]
 
+zsNeigh = 5
+# NbrRectangle({width}, {height}, {units})
+neighborhood = NbrRectangle(zsNeigh, zsNeigh, "CELL")
 
 # Check if output directory exists. Create a directory if one does not exist
 outFolder = path + "\\output.gdb"
@@ -35,21 +41,23 @@ if os.path.exists(outFolder):
 else: 
     arcpy.management.CreateFileGDB(path, "output.gdb")
     print('Created the output directory')
- 
- 
-def tallHills_lowValleys(inPainting, zsNeigh):
-    # NbrRectangle({width}, {height}, {units})
-    neighborhood = NbrRectangle(zsNeigh, zsNeigh, "CELL")
-    
+
+
+######################################################################################################### 
+# Count the number of tallhills / low valleys  
+def tallHills_lowValleys(inPainting, neighborhood, oneValPainting):
+    # Get name of current picture that is being evaluated
+    name = os.path.basename(inPainting)
+        
     # Calculate focal stats mean x3 
     smooth = arcpy.sa.FocalStatistics(inPainting, neighborhood, "MEAN")
     smoother = arcpy.sa.FocalStatistics(smooth, neighborhood, "MEAN")
     smoothest = arcpy.sa.FocalStatistics(smoother, neighborhood, "MEAN")
-    print("Smoooooth")
+    print(name + " smoothed")
     
     # Identify regions of great change by subtracting Mean x3 from original raster
-    deviations = RasterCalculator([inPainting, smoothest], ["x", "y"], "x-y", "FirstOf", "FirstOf")
-    print("Deviations created")
+    deviations = RasterCalculator([inPainting, smoothest], ["x", "y"], "x-y")
+    print(name + " deviations created")
     
     # Reclassify to identify tall hills (high positives) and low valleys (low negatives)
     # Hard-coded values should be changed in the future
@@ -57,33 +65,61 @@ def tallHills_lowValleys(inPainting, zsNeigh):
     # Using breaks defined by 5 quantile symbology 
     tallHills = Reclassify(deviations, "Value", "-3.497446 -0.431105 NODATA;-0.431105 -0.102569 NODATA;-0.102569 0.143834 NODATA;0.143834 0.444992 NODATA;0.444992 3.483955 1")
     lowValleys = Reclassify(deviations, "Value", "-3.497446 -0.431105 1;-0.431105 -0.102569 NODATA;-0.102569 0.143834 NODATA;0.143834 0.444992 NODATA;0.444992 3.483955 NODATA")
-    
-    print("Identifying your tall hills and low valleys")
+    print(name + " tall hills and low valleys identified")
     
     # Create zones for each individual hill / valley
     indvTallHills = RegionGroup(tallHills, "FOUR", "WITHIN", "ADD_LINK")
-    indvLowValleys = RegionGroup(lowValleys, "FOUR", "WITHIN", "ADD_LINK")
-    print("Seperate them!")
+    indvLowValleys = RegionGroup(lowValleys, "FOUR", "WITHIN", "ADD_LINK")  
+        
+    # Count the number of tall hills / low valleys in the painting
+    numTallHills = arcpy.sa.ZonalStatistics(oneValPainting, "Value", indvTallHills, "MAXIMUM")
+    numLowValleys = arcpy.sa.ZonalStatistics(oneValPainting, "Value", indvLowValleys, "MAXIMUM")
+    print(name + " tall hills and low valleys counted")
     
+    # Save intermediates for viewing 
+    oneValPainting.save(outFolder + "\\" + name + r"_oneValue")  
+    smooth.save(outFolder + "\\" + name + r"_smooth")
+    smoother.save(outFolder + "\\" + name + r"_smoother")
+    smoothest.save(outFolder + "\\" + name + r"_smoothest")
+    deviations.save(outFolder + "\\" + name + r"_deviations")
+    tallHills.save(outFolder + "\\" + name + r"_tallHills")
+    lowValleys.save(outFolder + "\\" + name + r"_lowValleys")
+    indvTallHills.save(outFolder + "\\" + name + r"_indvTallHills")
+    indvLowValleys.save(outFolder + "\\" + name + r"_indvLowValleys")
+    numTallHills.save(outFolder + "\\" + name + r"_numTallHills")
+    numLowValleys.save(outFolder + "\\" + name + r"_numLowValleys")
+    print(name + " outputs saved to disk")
+    
+    # Return objects for later use
+    return numTallHills, numLowValleys
+
+#########################################################################################################
+# Identify percentage of "rough" areas - essentially how many interactions between non-similar zones
+def pctRough(inPainting, neighborhood, areaOVP):
+    # Get name of current picture that is being evaluated
+    name = os.path.basename(inPainting)
+    
+    # Calculate the standard deviation (roughness)
+    roughness = arcpy.sa.FocalStatistics(inPainting, neighborhood, "STD")
+    roughness.save(outFolder + "\\" + name + r"_roughness")
+    
+    # Select only really rough zones
+    
+    
+    # Find total area of rough zone
+    
+    # Calculate area of rough zones as a percentage of total area
+    
+#########################################################################################################
+# Iterate through images to process
+for pic in pics:
     # Create single value painting raster
-    #painting1 = Reclassify(inPainting, "Value", RemapRange[0.1,10,1])
+    oneValPainting = Reclassify(pic, "Value", "0.100000 10 1")
+    # Calculate area of each individual painting
+    areaOVP = ZonalGeometry(oneValPainting, "Value", "AREA")
     
-    # Save outputs for viewing
-    smooth.save(outFolder + "\\smooth")
-    smoother.save(outFolder + "\\smoother")
-    smoothest.save(outFolder + "\\smoothest")
-    deviations.save(outFolder + "\\deviations")
-    tallHills.save(outFolder + "\\tallHills")
-    lowValleys.save(outFolder + "\\lowValleys")
-    indvTallHills.save(outFolder + "\\indvTallHills")
-    indvLowValleys.save(outFolder + "\\indvLowValleys")
-    print("Outputs saved! Have a look!")
-    
-tallHills_lowValleys(pic1, 5)
-    
-    
-    
-    
-    
+    #tallHills_lowValleys(pic, neighborhood, oneValPainting)
+    pctRough(pic, neighborhood, areaOVP)
+print("Outputs saved! Have a look!") 
     
     
