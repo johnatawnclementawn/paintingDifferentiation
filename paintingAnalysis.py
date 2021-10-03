b6ddf3d7215acdf44e636b097217d5bef093fe5e
johnatawnclementawn <johnathan.clementi1@gmail.com>
2021-10-03 10:28:08 -0400

    Functioning script, need more scoring values to differentiate pictures

--- paintingAnalysis.py
+++ paintingAnalysis.py
@@ -4,17 +4,16 @@
 # Automating Painting Differentiation
 
 import os, sys
+import numpy
 import arcpy
 from arcpy.sa import *
 from arcpy.ia.raster_functions import RasterCalculator
-from arcpy.sa.Functions import Reclassify, RegionGroup
-# from arcpy.sa.Functions import Reclassify
-# from arcpy.sa.ComplexArguments import RemapRange
- 
+
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = r'in_memory'
 # Check out the ArcGIS Spatial Analyst extension license
 arcpy.CheckOutExtension("Spatial")
+
 
 # Set Working Directory
 os.chdir(r'D:\Users\Johnathan\Google Drive\Grad School\PennDesign_MUSA\RasterGIS_UrbanAndEnvironmentalModeling\Week5\wk5Homework')
@@ -43,16 +42,69 @@ else:
     print('Created the output directory')
 
 
+
+######################################################################################################### 
+# Quantiles reclassification function courtesy of 
+# https://gis.stackexchange.com/questions/309808/classifying-quantiles-in-raster-dataset-with-arcpy
+def reclassify_by_quantiles(raster, quantiles):
+    desc = arcpy.Describe(raster)
+
+    # Get the quantile break points
+    percentiles = list()
+    for i in range(1, quantiles):
+        percentiles.append(i * (100.0/quantiles))
+
+    # Ensure that the raster is a raster object, in order to identify the minimum and
+    #   maximum values
+    if type(raster) != arcpy.Raster:
+        raster = arcpy.Raster(raster)
+
+    value_minimum = raster.minimum
+    value_maximum = raster.maximum
+
+    # Identify a value that does not occur in the raster
+    null_value = value_minimum - 1
+
+    # If the raster is not an integer type, then there is no need to futz with NaNs
+    if desc.pixelType.startswith('F'):
+        arr = arcpy.RasterToNumPyArray(raster, nodata_to_value=numpy.NaN)
+    # Since integer arrays can't contain NaNs, you must do wackiness
+    else:
+        # Convert to an array, setting NoData cells to the unique value
+        arr = arcpy.RasterToNumPyArray(raster, nodata_to_value=null_value)
+        # Convert the array of integers to an array of floats
+        arr = arr.astype('float')
+        # Replace the placeholder null value with NaNs
+        arr[arr==null_value] = numpy.NaN
+
+    # Compile the quantile breaks
+    breakpoints = list(numpy.nanpercentile(arr, percentiles))
+    breakpoints.insert(0, value_minimum)
+    breakpoints.append(value_maximum)
+
+    # You no longer need the array...though you could do the remaining calculations
+    #   on the array instead of one the raster
+    del(arr)
+
+    # Map the ranges to class numbers
+    remap_table = list()
+    for index, breakpt in enumerate(breakpoints[:-1]):
+        remap_table.append([breakpt, breakpoints[index+1], index+1])
+    remap = RemapRange(remap_table)
+
+    result = Reclassify(raster, 'Value', remap)
+    return result
+
+
+
 ######################################################################################################### 
 # Count the number of tallhills / low valleys  
-def tallHills_lowValleys(inPainting, neighborhood, oneValPainting):
-    # Get name of current picture that is being evaluated
-    name = os.path.basename(inPainting)
+def tallHills_lowValleys(inPainting, name, neighborhood, oneValPainting):
         
     # Calculate focal stats mean x3 
-    smooth = arcpy.sa.FocalStatistics(inPainting, neighborhood, "MEAN")
-    smoother = arcpy.sa.FocalStatistics(smooth, neighborhood, "MEAN")
-    smoothest = arcpy.sa.FocalStatistics(smoother, neighborhood, "MEAN")
+    smooth = FocalStatistics(inPainting, neighborhood, "MEAN")
+    smoother = FocalStatistics(smooth, neighborhood, "MEAN")
+    smoothest = FocalStatistics(smoother, neighborhood, "MEAN")
     print(name + " smoothed")
     
     # Identify regions of great change by subtracting Mean x3 from original raster
@@ -63,8 +115,9 @@ def tallHills_lowValleys(inPainting, neighborhood, oneValPainting):
     # Hard-coded values should be changed in the future
     # RemapRange args are ([startValue,endValue,newValue],...)
     # Using breaks defined by 5 quantile symbology 
-    tallHills = Reclassify(deviations, "Value", "-3.497446 -0.431105 NODATA;-0.431105 -0.102569 NODATA;-0.102569 0.143834 NODATA;0.143834 0.444992 NODATA;0.444992 3.483955 1")
-    lowValleys = Reclassify(deviations, "Value", "-3.497446 -0.431105 1;-0.431105 -0.102569 NODATA;-0.102569 0.143834 NODATA;0.143834 0.444992 NODATA;0.444992 3.483955 NODATA")
+    deviationsQuantiles = reclassify_by_quantiles(deviations, 5)
+    tallHills = Reclassify(deviationsQuantiles, "VALUE", "1 NODATA;2 NODATA;3 NODATA;4 NODATA;5 1", "DATA")
+    lowValleys = Reclassify(deviationsQuantiles, "VALUE", "1 1;2 NODATA;3 NODATA;4 NODATA;5 NODATA", "DATA")
     print(name + " tall hills and low valleys identified")
     
     # Create zones for each individual hill / valley
@@ -72,8 +125,8 @@ def tallHills_lowValleys(inPainting, neighborhood, oneValPainting):
     indvLowValleys = RegionGroup(lowValleys, "FOUR", "WITHIN", "ADD_LINK")  
         
     # Count the number of tall hills / low valleys in the painting
-    numTallHills = arcpy.sa.ZonalStatistics(oneValPainting, "Value", indvTallHills, "MAXIMUM")
-    numLowValleys = arcpy.sa.ZonalStatistics(oneValPainting, "Value", indvLowValleys, "MAXIMUM")
+    numTallHills = ZonalStatistics(oneValPainting, "Value", indvTallHills, "MAXIMUM")
+    numLowValleys = ZonalStatistics(oneValPainting, "Value", indvLowValleys, "MAXIMUM")
     print(name + " tall hills and low valleys counted")
     
     # Save intermediates for viewing 
@@ -88,38 +141,77 @@ def tallHills_lowValleys(inPainting, neighborhood, oneValPainting):
     indvLowValleys.save(outFolder + "\\" + name + r"_indvLowValleys")
     numTallHills.save(outFolder + "\\" + name + r"_numTallHills")
     numLowValleys.save(outFolder + "\\" + name + r"_numLowValleys")
-    print(name + " outputs saved to disk")
+    print(name + " th/lv outputs saved to disk")
     
     # Return objects for later use
-    return numTallHills, numLowValleys
+    return [numTallHills, numLowValleys]
+
+
 
 #########################################################################################################
 # Identify percentage of "rough" areas - essentially how many interactions between non-similar zones
-def pctRough(inPainting, neighborhood, areaOVP):
-    # Get name of current picture that is being evaluated
-    name = os.path.basename(inPainting)
+def pctRough(inPainting, name, neighborhood, oneValPainting, areaOVP):
     
-    # Calculate the standard deviation (roughness)
-    roughness = arcpy.sa.FocalStatistics(inPainting, neighborhood, "STD")
-    roughness.save(outFolder + "\\" + name + r"_roughness")
+    # Calculate the standard deviation (rough)
+    rough = FocalStatistics(inPainting, neighborhood, "STD")
+    rough.save(outFolder + "\\" + name + r"_rough")
     
-    # Select only really rough zones
-    
+    # Reclass upper quantile
+    roughQuantiles = reclassify_by_quantiles(rough, 5)
+    veryRough = Reclassify(roughQuantiles, "VALUE", "1 NODATA;2 NODATA;3 NODATA;4 NODATA;5 1", "DATA")
+    veryRough.save(outFolder + "\\" + name + r"_veryRough")    
     
     # Find total area of rough zone
+    vRoughArea = ZonalGeometry(veryRough, "VALUE", "AREA")
+    vRoughArea.save(outFolder + "\\" + name + r"_vRoughArea")
     
     # Calculate area of rough zones as a percentage of total area
+    maxRoughArea = ZonalStatistics(oneValPainting, "VALUE", vRoughArea, "MAXIMUM")
+    pctRoughArea = RasterCalculator([maxRoughArea, areaOVP], ["x", "y"], "(x/y)*1000")
+    areaOVP.save(outFolder + "\\" + name + r"_areaOVP")
+    pctRoughArea.save(outFolder + "\\" + name + r"_pctRoughArea")
+    print(name + " pct rough area calculated")
+
+    return pctRoughArea
+
+
+
+#########################################################################################################
+# Final Score Calculation 
+def scoreCalculator(name, numTallHills, numLowValleys, pctRoughArea):
+    finalScore = RasterCalculator([numTallHills, numLowValleys, pctRoughArea], ["x", "y", "z"], "x+y+z")
+    finalScore.save(outFolder + "\\" + name + r"_finalScore")
+    print(name + " final score calculated")
+    
+    return finalScore
+ 
+
+
+#########################################################################################################
+# Use main function to pass return values between functions
+def main():
+    numTallHills, numLowValleys = tallHills_lowValleys(pic, name, neighborhood, oneValPainting)
+    pctRoughArea = pctRough(pic, name, neighborhood, oneValPainting, areaOVP)
+    scoreCalculator(name, numTallHills, numLowValleys, pctRoughArea)
+
+
     
 #########################################################################################################
 # Iterate through images to process
 for pic in pics:
+    # Get name of current picture that is being evaluated
+    name = os.path.basename(pic)
+        
     # Create single value painting raster
     oneValPainting = Reclassify(pic, "Value", "0.100000 10 1")
+    
     # Calculate area of each individual painting
     areaOVP = ZonalGeometry(oneValPainting, "Value", "AREA")
     
-    #tallHills_lowValleys(pic, neighborhood, oneValPainting)
-    pctRough(pic, neighborhood, areaOVP)
+    main()
+    
+    
+        
 print("Outputs saved! Have a look!") 
     
     
